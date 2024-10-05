23499d9f82040d326030d8e2bde903b63f2de2ed
xeon <tommyvercetti1947@gmail.com>
2024-10-05 14:30:20 +0530

    same as previous commit

---
+++ assignment5/problem4.py
@@ -0,0 +1,42 @@
+xlist=[]
+N=int(input("enter the value of N:"))
+for i in range(N):
+    x=input(f"enter the value of x-{(i+1)}:")
+    xlist.append(float(x))
+
+
+xlist.sort()
+n=len(xlist)
+if n%2==1:
+    median=xlist[((n+1)//2)-1]
+else:
+    median=((xlist[(n//2)-1])+(xlist[((n+2)//2)-1]))/2
+
+
+list2=xlist.copy()
+list2.append(median)
+list2.append(xlist[(n//2)-1])
+list2.append(xlist[((n+2)//2)-1])
+sumlist=[]
+def sumlistcaller(j):
+    sum0=0
+    for i in range(len(xlist)):
+        sum0+=abs((list2[j]-xlist[i]))
+    sumlist.append(sum0)
+
+
+for j in range(len(list2)):
+    sumlistcaller(j)
+
+
+finallist=[]
+
+for z in range(len(list2)):
+    finallist.append([list2[z],sumlist[z]])
+
+print(finallist)
+
+
+for c in range(len(list2)):
+    print(f"the value of S for x-{c+1} is: {finallist[c][1]}")
+print(f"The median is [{xlist[(n//2)-1]},{xlist[((n+2)//2)-1]}].\nx-{len(list2)} is the median and we can see the minimum value of S comes at the median.")
